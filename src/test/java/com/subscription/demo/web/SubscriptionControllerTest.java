309d08a63768b5958553f00d5eae901db2207e07
ceciamura <cecilia.amura88@gmail.com>
2026-06-15 14:14:40 +0100

    add pagination sorting and tests

--- src/test/java/com/subscription/demo/web/SubscriptionControllerTest.java
+++ src/test/java/com/subscription/demo/web/SubscriptionControllerTest.java
@@ -5,10 +5,14 @@ import com.subscription.demo.domain.Plan;
 import com.subscription.demo.domain.Subscription;
 import com.subscription.demo.web.request.CreateSubscriptionRequest;
 import com.subscription.demo.web.request.UpdateSubscriptionRequest;
+import com.subscription.demo.web.response.PlanResponse;
+import com.subscription.demo.web.response.SubscriptionResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -142,4 +146,51 @@ class SubscriptionControllerTest {
                 .andExpect(jsonPath("$.plan.name").value("aaa"))
                 .andExpect(jsonPath("$.plan.monthlyPrice").value(10.0));
     }
-}
+
+        @Test
+        void shouldReturnSubscriptionPage() throws Exception {
+            Plan plan = new Plan("2", "aaa", 10.0);
+
+            Subscription subscription = new Subscription(
+                    "1","aa@aa.com" , plan);
+
+
+            Page<Subscription> page =
+                    new PageImpl<>(List.of(
+                            new Subscription(
+                                    "1",
+                                    "hola@hola.com",
+                                    new Plan("1", "Premium", 20.0)
+                            )
+                    ));
+
+            Mockito.when(subscriptionService.getSubscriptionPage(
+                    0,
+                    2,
+                    "customerEmail",
+                    "desc"
+            )).thenReturn(page);
+
+            mockMvc.perform(
+                            get("/subscriptions/page")
+                                    .param("page", "0")
+                                    .param("size", "2")
+                                    .param("sortBy", "customerEmail")
+                                    .param("direction", "desc")
+                    )
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.content[0].id").value("1"))
+                    .andExpect(jsonPath("$.content[0].customerEmail").value("hola@hola.com"))
+                    .andExpect(jsonPath("$.content[0].plan.id").value("1"));
+
+            Mockito.verify(subscriptionService).getSubscriptionPage(
+                    0,
+                    2,
+                    "customerEmail",
+                    "desc"
+            );
+        }
+
+    }
+
+
